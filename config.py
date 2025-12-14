a9547c2c249518f568d07a5f7279f2e080bf3478
stxrman404 <mr.stxrman@outlook.com>
2025-12-14 21:48:07 +0000

    Update config.py

--- config.py
+++ config.py
@@ -2,6 +2,7 @@
 Configuration file for StarSearch Client
 Edit SERVER_HOST to match your main computer's IP address
 """
-SERVER_HOST = "31.48.79.68"  # Change this to your main computer's IP address
+SERVER_HOST = "100.98.252.33"  # Change this to your main computer's IP address
 SERVER_PORT = 8888
+
 
